ef912cf484e2df923f3f25e328881d03c73fc428
Carlos Murcia <carlos.murciam@perficient.com>
2022-09-14 18:05:50 -0500

    fixing the rout

--- build/precache-manifest.be2a020a029d6d58f2d8a20af3519aab.js
+++ build/precache-manifest.be2a020a029d6d58f2d8a20af3519aab.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "72b50efc264e2b6c127e",
+    "revision": "e70b73eedb99dad897e9",
     "url": "/meetup_app/static/css/main.4b2ac353.chunk.css"
   },
   {
-    "revision": "72b50efc264e2b6c127e",
-    "url": "/meetup_app/static/js/main.d5adfbde.chunk.js"
+    "revision": "e70b73eedb99dad897e9",
+    "url": "/meetup_app/static/js/main.3e83520e.chunk.js"
   },
   {
     "revision": "ae3a93869b2f1b3c010b",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/meetup_app/static/js/2.c15e8c33.chunk.js"
   },
   {
-    "revision": "a63191b4d3807a28c0b5a70e5608710e",
+    "revision": "28c6700be193def2b75f78fc1ba0f8a9",
     "url": "/meetup_app/index.html"
   }
 ];
