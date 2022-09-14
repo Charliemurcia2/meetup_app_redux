63383c71891a3ad2d3763bfe62dfe6c79e533c6d
Carlos Murcia <carlos.murciam@perficient.com>
2022-09-14 17:37:25 -0500

    let build folder

---
+++ build/precache-manifest.31d0e1982cb153061c9451506efb38bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "72b50efc264e2b6c127e",
+    "url": "/meetup_app/static/css/main.4b2ac353.chunk.css"
+  },
+  {
+    "revision": "72b50efc264e2b6c127e",
+    "url": "/meetup_app/static/js/main.d5adfbde.chunk.js"
+  },
+  {
+    "revision": "ae3a93869b2f1b3c010b",
+    "url": "/meetup_app/static/js/runtime~main.a6d56fb2.js"
+  },
+  {
+    "revision": "c49b54354aa2095fd703",
+    "url": "/meetup_app/static/js/2.c15e8c33.chunk.js"
+  },
+  {
+    "revision": "a63191b4d3807a28c0b5a70e5608710e",
+    "url": "/meetup_app/index.html"
+  }
+];
