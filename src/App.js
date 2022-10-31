51fe954de3138ef7430596814aeffb799458e894
Carlos Murcia <carlos.murciam@perficient.com>
2022-10-30 20:58:47 -0500

    adding lazy load

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Route, Switch } from "react-router-dom";
 
-import AllMeetups from "./routes/AllMeetups";
-import Favorites from "./routes/Favorites";
-import NewMeetup from "./routes/NewMeetup";
 import Users from "./routes/Users";
 import Layout from "./components/layout/Layout";
+import Loading from "./components/layout/Loading";
+
+const AllMeetups = lazy(() => import("./routes/AllMeetups"));
+const Favorites = lazy(() => import("./routes/Favorites"));
+const NewMeetup = lazy(() => import("./routes/NewMeetup"));
 
 const App = () => {
   return (
     <Layout>
-      <Switch>
-        <Route path="/" exact>
-          <AllMeetups />
-        </Route>
-        <Route path="/new-meetup">
-          <NewMeetup />
-        </Route>
-        <Route path="/favorites">
-          <Favorites />
-        </Route>
-        <Route path="/users">
-          <Users />
-        </Route>
-      </Switch>
+      <Suspense fallback={<Loading />}>
+        <Switch>
+          <Route path="/" exact>
+            <AllMeetups />
+          </Route>
+          <Route path="/new-meetup">
+            <NewMeetup />
+          </Route>
+          <Route path="/favorites">
+            <Favorites />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 };
